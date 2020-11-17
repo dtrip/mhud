60dc48b7760b8119e30b1e84fe4c032151343f2f
Dtrip <d@idgen.co>
2020-11-17 06:25:00 -0800

    dev

---
+++ hud.lua
@@ -0,0 +1,12 @@
+
+function ()
+    local x,y,r = 0, 0, 1
+
+        for i = 1, 360 do
+            local angle = i * math.pi / 100
+
+            local ptx, pty = x + r * math.cos(angle), y + r * math.sin(angle)
+            drawPoint(ptx, pty)
+        end
+    return true
+end
--- mhud.py
+++ mhud.py
@@ -2,21 +2,41 @@ import signal
 from pynput.mouse import Controller, Button
 import sys
 import time
-
+import lupa
+from lupa import LuaRuntime
 
 mouse = Controller()
 px, py = (0,0)
 
-while True:
-    try:
-        x, y = mouse.position
-        if (x != px or y != py):
-            print("X:%s Y: %s" % (x,y))
 
-        time.sleep(0.1)
-        px = x
-        py = y
-    except KeyboardInterrupt:
-        print("SIGINT Caught")
-        sys.exit()
+class mhud:
 
+    args = None
+    lua = LuaRuntime(unpack_returned_tuples=False)
+    mouse = Controller()
+
+
+
+    def __init__(self):
+        pass
+
+    def run(self):
+
+        px, py = (0,0)
+        while True:
+            try:
+                x, y = mouse.position
+                if (x != px or y != py):
+                    print("X:%s Y: %s" % (x,y))
+
+                time.sleep(0.1)
+                px = x
+                py = y
+            except KeyboardInterrupt:
+                print("SIGINT Caught")
+                sys.exit()
+
+
+if __name__ == "__main__":
+    m = mhud()
+    m.run()
