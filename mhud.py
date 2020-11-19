6f89092e51ebb3e5aaf20f818e7846b0b2731342
Dtrip <dtrip@rtx.cx>
2020-11-18 23:34:26 -0800

    dev

--- mhud.py
+++ mhud.py
@@ -1,6 +1,7 @@
 import signal
 from pynput.mouse import Controller, Button
 import sys
+import os
 import time
 import lupa
 from lupa import LuaRuntime
@@ -18,23 +19,39 @@ class mhud:
 
 
     def __init__(self):
-        pass
+            pass
 
     def run(self):
 
         px, py = (0,0)
-        while True:
-            try:
+        try:
+            while True:
                 x, y = mouse.position
                 if (x != px or y != py):
                     print("X:%s Y: %s" % (x,y))
 
+
+
                 time.sleep(0.1)
                 px = x
                 py = y
-            except KeyboardInterrupt:
-                print("SIGINT Caught")
-                sys.exit()
+        except KeyboardInterrupt:
+            print("SIGINT Caught")
+            sys.exit()
+        except Exception as e:
+            print("ERROR: %s" % e)
+            sys.exit()
+
+    def getLua(self):
+        
+        lua = None
+
+        if (os.stat("hud.lua").st_size == 0):
+            raise Exception("Lua code is empty!")
+
+        with open("hud.lua") as fh:
+
+        
 
 
 if __name__ == "__main__":
